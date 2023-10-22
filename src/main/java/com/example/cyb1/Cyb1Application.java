d80992adca580620cab5819cf0d90135274eabc5
AdrianRzadek <mschxd@gmail.com>
2023-10-22 13:22:47 +0200

    Initial commit

---
+++ src/main/java/com/example/cyb1/Cyb1Application.java
@@ -0,0 +1,13 @@
+package com.example.cyb1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cyb1Application {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(Cyb1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cyb1/Cyb1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cyb1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cyb1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
