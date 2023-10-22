b1fca40fca7271806c398a90b6b922848d1a3614
AdrianRzadek <mschxd@gmail.com>
2023-10-22 13:52:35 +0200

    Password encoder added

--- src/main/java/com/example/cyb1/WebSecurityConfig.java
+++ src/main/java/com/example/cyb1/WebSecurityConfig.java
@@ -7,9 +7,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 @Configuration
 @EnableWebSecurity
 @EnableWebMvc
@@ -18,33 +21,37 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/", "/home").permitAll()
-                .requestMatchers("/admin/**").hasRole("ADMIN") // Dodaj dostęp do ścieżki /admin tylko dla użytkowników z rolą "ADMIN"
-                .anyRequest().authenticated()
-            )
-            .formLogin((form) -> form
-                .loginPage("/login")
-                .permitAll()
-            )
-            .logout((logout) -> logout.permitAll());
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/home").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN") // Dodaj dostęp do ścieżki /admin tylko dla
+                                                                       // użytkowników z rolą "ADMIN"
+                        .anyRequest().authenticated())
+                .formLogin((form) -> form
+                        .loginPage("/login")
+                        .permitAll())
+                .logout((logout) -> logout.permitAll());
 
         return http.build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-            .username("user")
-            .password("password")
-            .roles("USER")
-            .build();
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        UserDetails admin = User.withDefaultPasswordEncoder()
-            .username("admin")
-            .password("adminpassword")
-            .roles("ADMIN")
-            .build();
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder.encode("adminpassword"))
+                .roles("ADMIN")
+                .build();
 
         return new InMemoryUserDetailsManager(user, admin);
     }
