ed45ea71221da8d63dad896bee1f1ad7832d5fdb
AdrianRzadek <mschxd@gmail.com>
2023-11-22 00:03:02 +0100

    login attempts

---
+++ src/main/java/com/example/cyb1/controller/AuthenticationFailureListener.java
@@ -0,0 +1,31 @@
+package com.example.cyb1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
+import org.springframework.stereotype.Component;
+
+import com.example.cyb1.service.LoginAttemptService;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class AuthenticationFailureListener implements 
+  ApplicationListener<AuthenticationFailureBadCredentialsEvent> {
+
+    @Autowired
+    private HttpServletRequest request;
+
+    @Autowired
+    private LoginAttemptService loginAttemptService;
+
+    @Override
+    public void onApplicationEvent(AuthenticationFailureBadCredentialsEvent e) {
+        final String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader == null || xfHeader.isEmpty() || !xfHeader.contains(request.getRemoteAddr())) {
+            loginAttemptService.loginFailed(request.getRemoteAddr());
+        } else {
+            loginAttemptService.loginFailed(xfHeader.split(",")[0]);
+        }
+    }
+}
